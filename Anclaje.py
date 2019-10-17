ec8899deb655e69178073ee7bae50d0668ffc394
Jose Raul Gallardo <joseraul.gallardo97@gmail.com>
2019-10-16 22:02:03 -0500

    Subiendo archivos
    Librería y otros recursos necesarios para testear

---
+++ Anclaje.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 08:28:42 2019
+
+@author: gallardj
+"""
+
+def pareto(tickers):
+    # Grafico pareto para analizar principales divisas transaccionadas
+    """
+    :param tickers: columna de dataframe con los tickers de las divisas
+    :return grafica
+    
+    debbuging
+    tickers = df['Symbol']
+    """
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import PercentFormatter
+    
+    # Construcción del DataFrame a utilizar para el pareto
+    pareto = pd.DataFrame({'Percentage':(tickers.value_counts()/tickers.count())*100}) #% de participación
+    pareto = pareto.sort_values(by='Percentage',ascending=False) #Sort de mayor a menor % para línea
+    pareto['TransactionC'] = list(tickers.value_counts()) #Conteo de valores para gráfico de barras
+    
+    # Definicipón del gráfico
+    fig, ax = plt.subplots()
+    ax.bar(pareto.index, pareto['TransactionC'], color="g") # Barras del gráfico
+    ax2 = ax.twinx() # Tener dos valores distintos en los ejes y
+    ax2.plot(pareto.index, pareto['Percentage'].cumsum(), color="k", marker="o") #Graficando la línea
+    ax2.yaxis.set_major_formatter(PercentFormatter()) #Formato de porcentaje al eje de línea
+    ax.set_xlabel('Currency') #Título del xlabel
+    ax.set_ylabel('Count')
+    ax2.set_ylabel('Percentage')
+    
+    ax.tick_params(axis='y', colors='g') #Color del eje de las barras para relacionar los valores
+    ax2.tick_params(axis='y', colors='k') #Color del eje de las líneas para relacionar valores
+    plt.setp(ax.get_xticklabels(), rotation=40, horizontalalignment='right') #Rotando etiquetas para que sean vistas
+    plt.title('Proporción de las divisas')
+    
+    plt.grid()
+    plt.show()
+    
+    return fig
---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 21:53:59 2019
+
+@author: gallardj
+"""
+
+import function_json2
+import Anclaje
+
+df = f_datosent(p0_archivo = 'archivo_tradeview_2.xlsx')
+
+test = Anclaje.pareto(tickers = df['Symbol'])
